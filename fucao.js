74ad8bd1591aeeb8a1cbf09754f9f796957dc4a2
Adão D. Nicolau <60399230+Nicolaspz@users.noreply.github.com>
2020-10-27 15:29:32 +0100

    adicionei todos arquivos via upload

---
+++ fucao.js
@@ -0,0 +1,32 @@
+$(document).ready(function()
+
+{
+  
+
+  $('.sub').click(()=>{
+    $('.sub-menu-estilo').slideToggle();
+   
+  
+    });
+      
+$('.icobars').click(()=>{
+  $('.menu-mobile').slideToggle();
+});
+/*
+
+
+$('.menu a').on('click',function(){
+ 
+  var href = $(this).attr('href');
+  var ofsetTTop = $(href).offset().top - 65;
+   $('html, body').animate({'scrollTop':ofsetTTop},2000);
+  });
+  $('.up').click(()=>{
+    $('html, body').animate({'scrollTop':0},2000);
+  });
+  $('.menu-mobile a').on('click',function(){
+    $('.menu-mobile').slideToggle();
+  })
+*/
+
+});
